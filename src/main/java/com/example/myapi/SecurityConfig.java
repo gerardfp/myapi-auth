c79cdec899882713b029cb3af81c1d7659f78883
gerardfp <gerardfp@gmail.com>
2021-11-02 15:16:29 +0100

    .

--- src/main/java/com/example/myapi/SecurityConfig.java
+++ src/main/java/com/example/myapi/SecurityConfig.java
@@ -1,35 +1,45 @@
 package com.example.myapi;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .cors()
                 .and()
-                    .anonymous()
-                .and()
-                    .authorizeRequests()
-                        .antMatchers(HttpMethod.GET,"/hello/**")
-                        .permitAll()
-                .and()
                 .authorizeRequests()
-                    .antMatchers(HttpMethod.GET, "/movies/**")
-                    .hasAuthority("SCOPE_read")
-                    .antMatchers(HttpMethod.POST, "/actors/**")
-                    .hasAuthority("SCOPE_write")
-
-//                .anyRequest()
-//                .authenticated()
-
+                .mvcMatchers("/hello").permitAll()
+                .mvcMatchers("/movies").authenticated()
                 .and()
-                .oauth2ResourceServer()
-                .jwt();
+                .oauth2ResourceServer().jwt();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
     }
 }
