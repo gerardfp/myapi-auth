c5d50a0273aa59ac3062ba83a7a20d0affd67a1a
gerardfp <gerardfp@gmail.com>
2021-11-02 12:09:39 +0100

    .

--- src/main/java/com/example/myapi/SecurityConfig.java
+++ src/main/java/com/example/myapi/SecurityConfig.java
@@ -10,15 +10,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
+        http
+                .cors()
+                .and()
+                    .anonymous()
+                .and()
+                    .authorizeRequests()
+                        .antMatchers(HttpMethod.GET,"/hello/**")
+                        .permitAll()
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/movies")
-                .hasAuthority("SCOPE_read")
-                .antMatchers(HttpMethod.POST, "/actors/**")
-                .hasAuthority("SCOPE_write")
-                .anyRequest()
-                .authenticated()
+                    .antMatchers(HttpMethod.GET, "/movies/**")
+                    .hasAuthority("SCOPE_read")
+                    .antMatchers(HttpMethod.POST, "/actors/**")
+                    .hasAuthority("SCOPE_write")
+
+//                .anyRequest()
+//                .authenticated()
+
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
---
+++ src/main/java/com/example/myapi/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.myapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @GetMapping("/")
+    String helloWorld(){
+        return "Hello World!";
+    }
+}
